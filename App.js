0e6ce2478ef668cb098188806cb659998c4c05dc
nunkAlfath <nunkalfath@gmail.com>
2021-06-30 10:36:38 +0700

    misahin todolist

--- App.js
+++ App.js
@@ -1,45 +1,40 @@
-import React from 'react';
-import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {View, Text} from 'react-native';
+import React from 'react';
+import {Text, View} from 'react-native';
+import 'react-native-gesture-handler';
 import Login from './src/screens/Login';
-import Colorcollection from './src/asset/Colorcollection';
 import UpdateState from './src/screens/UpdateState';
-import UpStateInung from './src/screens/UpStateInung';
-import {TodoList} from './src/TodosApp/TodoList';
 
-// function LoginScreen({navigation}) {
-//   return (
-//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-//       <Text>Login Screen</Text>
-//     </View>
-//   );
-// }
+function LoginScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+}
 
-// function AccountScreen() {
-//   return (
-//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-//       <Text>Account</Text>
-//     </View>
-//   );
-// }
+function AccountScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Account</Text>
+    </View>
+  );
+}
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <TodoList />
-      {/* <Stack.Navigator>
+      <Stack.Navigator>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Account" component={UpdateState} />
         <Stack.Screen name="Login Screen" component={LoginScreen} />
         <Stack.Screen name="Account Screen" component={AccountScreen} />
-      </Stack.Navigator> */}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
-
